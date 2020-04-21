b826ec223cf619cae86f3f33f7d41cc4d4835869
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2020-04-21 15:33:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const { prefix , token } = require("./config.json")
 
-const { prefix, token } = require("./config.json")
+var stock = "0";
 
-var sending = false;
-var a = 1;
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setActivity("Moon", {type: "LISTENING"})
-});
+  client.user.setActivity("!help", {type: "PLAYING"})
+})
 
 client.on('message', (receivedMessage) => {
-  if (receivedMessage.author == client.user) {
-    return
-  }
+    if (receivedMessage.author == client.user) {
+        return
+    }
 
+    let args = receivedMessage.content.substring(prefix.length).split(" ");
     var getUser = receivedMessage.author.toString() + ' ';
 
-    if (receivedMessage.content === `${prefix}help`) {
-        receivedMessage.channel.send(`**Command available** \n*${prefix}discord* - Get the discord server generator \n*${prefix}help* - Help command`)
-      }
-
-    if (receivedMessage.content === `${prefix}alt`) {
-      if(a = 1) {
-      receivedMessage.channel.send('asd')
-      sending = true;
-    } else if (sending = false) {
-      receivedMessage.channel.send(getUser + 'This command is disabled in here!')
+    if (receivedMessage.content === prefix) {
+	receivedMessage.channel.send(getUser + 'Type `!help` for command')
     }
-  } else
-  receivedMessage.author.send('Tao đéo có quyền dcm')
+
+    switch(args[0]) {
+      case 'alt':
+        if(stock > 0 ) {
+	var string = `oliver.goldie6@gmail.com:Ollie1997
+aochoa1968@verizon.net:5handguns`
+
+        var words = string.split('\n');
+        let random = words[Math.floor(Math.random()*words.length)];
+
+	   receivedMessage.author.send(`${random}`);
+     } else
+          receivedMessage.channel.send(getUser + 'Sorry, but there are currently no alts in stock.')
+	break;
+
+      case 'alts':
+        if(stock > 0) {
+          receivedMessage.channel.send(getUser + '**' + stock + '** ' + 'alt(s) in stock.')
+        } else
+          receivedMessage.channel.send(getUser + 'No alt in stock.')
+      break;
+
+      case 'help':
+        receivedMessage.channel.send(`__Here is ommand available__: \n*${prefix}alt* - Get an alt \n*${prefix}alts* - View alts are available \n*${prefix}help* - Help command`)
+      break;
+    }
 });
 
-client.login(process.env.BOT_TOKEN);
+
+client.login(token)
