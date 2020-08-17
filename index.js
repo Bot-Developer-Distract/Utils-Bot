6c1a65b0ac585b4498376e05368fae6262202264
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2020-08-17 20:19:27 +0700

    Fix something

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const { prefix } = require("./config.json")
+var prefix = "!";
 usedCommandRecently4 = new Set();
 
 client.on('ready', () => {
